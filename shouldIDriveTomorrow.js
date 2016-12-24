cc3f0eb215c2c7014dc252963c543602a0101fcd
Dino <bra.scene@gmail.com>
2016-12-24 19:37:03 +0100

    Update shouldIDriveTomorrow.js

--- shouldIDriveTomorrow.js
+++ shouldIDriveTomorrow.js
@@ -1,3 +1,6 @@
-(function shouldIDriveTomorrow(myRegTable="E89-T-394"){
-        return myRegTable.split("-")[2].split("")[2]%2===0
+(function shouldIDriveTomorrow(myRegTable="E96-T-234"){
+	var initialDay = 24;
+	var currentDay = new Date().getDate();
+	var result = myRegTable.split("-")[2].split("")[2]%2===0;
+	return (currentDay-initialDay)%2===0 ? result : !result; 
 }())
